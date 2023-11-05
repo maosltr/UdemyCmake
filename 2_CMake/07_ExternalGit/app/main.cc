53f6c11a51fbc2343f489727f2cd9e2b7cdff6ef
Marouane Chouhou <marouane.chouhou@outlook.com>
2023-11-05 15:12:27 +0100

    External librares: Fetch Content
    Fetch the content of an external CMake library

--- 2_CMake/07_ExternalGit/app/main.cc
+++ 2_CMake/07_ExternalGit/app/main.cc
@@ -1,18 +1,29 @@
 #include <iostream>
 
-#include <nlohmann/json.hpp>
 
 #include "config.hpp"
 #include "my_lib.h"
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <nlohmann/json.hpp>
+#include <spdlog/spdlog.h>
 
 int main()
 {
     std::cout << project_name << '\n';
     std::cout << project_version << '\n';
 
-    std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "."
-              << NLOHMANN_JSON_VERSION_MINOR << "."
+    std::cout << "JSON Lib Version:" << NLOHMANN_JSON_VERSION_MAJOR << "." << NLOHMANN_JSON_VERSION_MINOR << "."
               << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+    std::cout << "FMT Lib Version:" << FMT_VERSION << "\n";
+
+    std::cout << "CXXOPTS Lib Version:" << CXXOPTS__VERSION_MAJOR << "." << CXXOPTS__VERSION_MINOR << "."
+              << CXXOPTS__VERSION_PATCH << "\n";
+
+    std::cout << "SPDLOG Lib Version:" << SPDLOG_VER_MAJOR << "." << SPDLOG_VER_MINOR << "." << SPDLOG_VER_PATCH
+              << "\n";
+
 
     print_hello_world();
 
